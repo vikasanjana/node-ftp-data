eef26cb86e6d6bc49d0d4f4eae53dd9c8f0ccd47
vikasanjana <va6903@gmail.com>
2024-12-30 16:46:00 +0530

    Create app.js file

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.post("/ftp/download", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
